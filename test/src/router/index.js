2a429f60ded2d80e6ee3dfd77aa65f5517a15739
ZouLB <137919091@qq.com>
2018-09-12 19:01:21 +0800

    backup9-12

---
+++ test/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const labelSystem = resolve => require(['@/components/labelSystem'],resolve);
+const systemDetail = resolve => require(['@/components/systemDetail'],resolve);
+const baseStation = resolve => require(['@/components/baseStation'],resolve);
+const labelCategory = resolve => require(['@/components/labelCategory'],resolve);
+
+Vue.use(Router)
+
+export default new Router({
+routes: [
+
+	{path:'/',redirect:'/labelSystem'},
+	{path:'/labelSystem',component:labelSystem},
+	{path:'/systemDetail',component:systemDetail},
+	{path:'/baseStation',component:baseStation},
+	{path:'/labelCategory',component:labelCategory},
+			
+]
+})
